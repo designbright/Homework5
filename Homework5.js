6e051d9df8f793f02bc74bd7b0f7233db0952e17
Kelly Miller <ukmills12@gmail.com>
2017-10-09 14:25:10 -0400

    Removed spaces in JS file

--- Homework5.js
+++ Homework5.js
@@ -1,6 +1,3 @@
-
-
-
 // The below function does not work. The variable twoPlusTwo gets set to undefined. Refactor the function to make it work.
 //
 // function addNumbers(numberA, numberB) {
@@ -11,14 +8,12 @@
 //
 // console.log(twoPlusTwo) // 4
 
-
 function addNumbers(a, b){
   return (a + b);
 }
 
 var twoPlustwo = addNumbers(2,2);
 console.log(twoPlustwo);
-
 
 // Write a function called yell that logs out an uppercase version of a string.
 
@@ -35,8 +30,8 @@ console.log(uppercase);
 
 // Write a function called yell10 that uses your yell function to log out an uppercase version of a string 10 times.
 
-
 Write a function called longest that returns the longest of two input strings or arrays.
 
-
-Write a function called isVowel that takes a character (i.e. a string of length 1) and returns true if it is a vowel, uppercase or lowercase. The function should return false if the character is not a vowel.
+Write a function called isVowel that takes a character (i.e. a string of length 1) 
+and returns true if it is a vowel, uppercase or lowercase. 
+The function should return false if the character is not a vowel.
